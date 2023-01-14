b0ec07a344f6f64164185ceea665918aab526e40
Satyam Rawat <78161825+satyamrawat625@users.noreply.github.com>
2023-01-14 16:14:44 +0530

    1st commit
    Displays weather type , details , icon only , upto L7 module API

---
+++ app1.js
@@ -0,0 +1,29 @@
+const { response } = require("express")
+const express= require("express")
+const https= require("https")
+const app= express()
+
+app.get("/",function(req,res) {
+    const url="https://api.openweathermap.org/data/2.5/weather?q=clement%20town,uttarakhand&appid=92d41bf4ca65d59d538996ed2e007ccb&units=metric"//clement town
+    // const url="https://api.openweathermap.org/data/2.5/weather?q=kashipur&appid=92d41bf4ca65d59d538996ed2e007ccb&units=metric"//kashipur
+    
+    https.get(url,function (response) {
+        // console.log(response)
+        console.log(response.statusCode)//to see status code
+
+        response.on("data",function(data) {
+            const weatherData =JSON.parse(data) 
+            const temp= weatherData.main.temp
+            const weatherDescript= weatherData.weather[0].description
+            const icon= weatherData.weather[0].icon
+            const imgeurl= "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+            res.write("<p>The weather is currently "+weatherDescript+"</p>")
+            res.write("<h1>The temp in london is "+temp+" deg Celsius</h1>")
+            res.write("<img src="+imgeurl+ ">")
+            res.send()
+        })
+    })
+})
+app.listen(3000,function (){
+    console.log("Server is running  on port 3000")
+})
